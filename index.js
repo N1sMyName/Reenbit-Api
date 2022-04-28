97682f4efd24fc0cc099fd1b751d6bfd69f5d190
N1sMyName <78924117+N1sMyName@users.noreply.github.com>
2022-04-28 16:02:27 +0300

    working proto

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const fs = require("fs");
 const cors = require("cors");
 const products = require("./db/products");
 const categories = require("./db/categories");
-const app = express();
 const PORT = process.env.PORT;
+const app = express();
 app.use(cors());
 app.get("/products", (req, res) => {
-  fs.readFile("./db/products.js", (err, data) => {
-    if (err) {
-      console.log(`something went wrong`);
-    }
-    console.log(products);
-    res.send({ products });
-  });
+  console.log(`request`);
+  res.send(products);
 });
+
 app.get("/products/:id", (req, res) => {
   const id = req.params.id;
-  const product = products.filter((e) => +e.id === +id);
+  console.log(`product served`);
+  const product = products.find((e) => +e.id === +id);
   res.send(product);
 });
+
 app.get("/categories", (req, res) => {
-  console.log(categories);
+  console.log(`categories served`);
   res.send(categories);
 });
 
